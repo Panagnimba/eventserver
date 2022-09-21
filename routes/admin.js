815134662144ad02b75ac068411670928ff49a43
Panagnimba <panagnimba@gmail.com>
2022-09-21 02:00:13 +0200

    resolve imagekit

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,15 @@ let connection = require("../database/connection.js")
 let Menu = require("../database/models/menu.js")
 let Banner = require("../database/models/banner.js")
 let Event = require("../database/models/event.js");
-let ImageKit = require("../imagekit/imagekit.js"); // for file upload
+// for file upload//  let ImageKit = require("../imagekit/imagekit.js"); 
+
+var ImageKi = require("imagekit");
+
+var ImageKit = new ImageKi({
+    publicKey : "public_xGsp4GcRJJjwYluoGenupRC2gy4=",
+    privateKey : "private_sc+xzMyqqKwiwRuTHpQN2YKDSj0=",
+    urlEndpoint : "https://ik.imagekit.io/g8k0fkvg9/Events"
+});
 
 
 // connection to database
