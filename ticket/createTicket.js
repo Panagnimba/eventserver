06d76f925829a10b757209982cab193d2cf62366
Panagnimba <panagnimba@gmail.com>
2022-11-08 17:14:05 +0100

    ticket generation

--- ticket/createTicket.js
+++ ticket/createTicket.js
@@ -1,36 +1,40 @@
-const { createCanvas, loadImage } = require('canvas')
+const { registerFont,createCanvas, loadImage } = require('canvas')
 let fs = require('fs')
 var path = require('path');
 // 
 let width = 500;
 let height = 200
 // 
+registerFont('lobster.ttf', { family: 'lobster' })
 const canvas = createCanvas(width, height)
 const ctx = canvas.getContext('2d')
 
-ctx.font = 'bold 15pt Impact'
+ctx.font = 'bold 20pt lobster'
 
 async function createTicket(Pevtimg,pqrcode,peventtitle,pheure,plieu,pnomclient){
   let image = await loadImage("ticketTemplate.png")
-    ctx.drawImage(image, 0, 0,width,height)
+    await ctx.drawImage(image, 0, 0,width,height)
     // left event image
     let eventImage = await loadImage(Pevtimg)
-      ctx.drawImage(eventImage, 0, 0,200,height)
+      await ctx.drawImage(eventImage, 0, 0,200,height)
       
      let qrcode = await loadImage(pqrcode)
     
-        ctx.fillStyle = '#fff'
-        ctx.fillText(peventtitle, width/2,40)
+        ctx.fillStyle = '#f3f4f6'
+        ctx.strokeStyle = '#f3f4f6'
+ 
+        ctx.fillText(peventtitle, 210,40)
         
-        ctx.fillText(pheure, ((width+40)/2),(height/2))
+        ctx.strokeText(pheure, ((width+60)/2),(height/2))
     
-        ctx.font = '10pt Arial'
-        ctx.fillText(plieu, (width/2),(height-48))
-        ctx.fillText(pnomclient, (width/2),(height-15))
+        ctx.font = '10pt Impact'
+        ctx.fillText(plieu, (width/2),(height-50))
+        ctx.fillText(pnomclient, (width/2),(height-17))
     
-        ctx.drawImage(qrcode,(width-110),(height-110),100,100)
-        const buffer = canvas.toBuffer('image/png')
-        return buffer
+        await ctx.drawImage(qrcode,(width-110),(height-110),100,100)
+        // const buffer = canvas.toBuffer('image/png')
+        // fs.writeFileSync("./img.png",buffer)
+        return await canvas.toDataURL('image/png')
 }
-
+// createTicket('lenna.png','qrcode.png',"Floby en concert live des artistes","20h45","Stade municipa","Panagnimba")
 module.exports = createTicket;
